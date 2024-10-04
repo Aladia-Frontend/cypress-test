8d4851ba291ff5b64c710e37357135ecaa64c88b
Rambod Rahmani <rambodrahmani@aladia.io>
2024-10-04 15:15:04 +0200

    reset vue project to javascript

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
+import LocatorTest from "./components/LocatorTest.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: { label: "Go to Home" },
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    meta: { label: "Go to About" },
+  },
+  {
+    path: "/locator-test",
+    name: "LocatorTest",
+    component: LocatorTest,
+    meta: { label: "Go to Locator test" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
