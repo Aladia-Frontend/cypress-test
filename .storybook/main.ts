4362f4408aba36456903a4a6a675250c559d3b7b
Rambod Rahmani <rambodrahmani@aladia.io>
2024-10-04 10:40:33 +0200

    update packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,17 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/components/LocatorTest.stories.ts",
+    "../src/**/*.stories.ts",
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-actions",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
