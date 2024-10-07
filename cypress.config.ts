3626b83c180fdee8ec1f7a060963e6eb547023ac
rambod-rahmani <rambodrahmani@aladia.io>
2024-10-07 15:03:26 +0200

    fix applitools cypress e2e tests integration

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import eyesPlugin from "@applitools/eyes-cypress";
+import { startDevServer } from "@cypress/vite-dev-server";
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+export default eyesPlugin(
+  defineConfig({
+    e2e: {
+      baseUrl: "http://localhost:8080",
+      setupNodeEvents(on, config) {
+        on("file:preprocessor", vitePreprocessor());
+        on("dev-server:start", (options) => startDevServer({ options }));
+        return config;
+      },
+      specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    },
+    component: {
+      devServer: {
+        framework: "vue",
+        bundler: "vite",
+      },
+    },
+  })
+);
